406231e7e8eda6f098026da5c8e18a9fb4591255
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-10-23 17:09:36 +0100

    Produce sqlite db with migration file, populate and query with demo values

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::{migrate::MigrateDatabase, FromRow, Row, Sqlite, SqlitePool};
+
+const DB_URL: &str = "sqlite://sqlite.db";
+
+#[derive(Clone, FromRow, Debug)]
+struct InsertionDevice {
+    uuid: i64,
+    poles: i64,
+}
+
+#[tokio::main]
+async fn main() {
+    if !Sqlite::database_exists(DB_URL).await.unwrap_or(false) {
+        println!("Creating database {}", DB_URL);
+        match Sqlite::create_database(DB_URL).await {
+            Ok(_) => println!("Created db"),
+            Err(error) => panic!("error: {}", error),
+        }
+    } else {
+        println!("Database already exists");
+    }
+
+    let db = SqlitePool::connect(DB_URL).await.unwrap();
+
+    let create_dir = std::env::var("CARGO_MANIFEST_DIR").unwrap();
+    let migrations = std::path::Path::new(&create_dir).join("./migrations");
+
+    let migration_results = sqlx::migrate::Migrator::new(migrations)
+        .await
+        .unwrap()
+        .run(&db)
+        .await;
+
+    match migration_results {
+        Ok(_) => println!("Migration success"),
+        Err(error) => {
+            panic!("error: {}", error);
+        }
+    }
+    println!("migration: {:?}", migration_results);
+
+    let result = sqlx::query(
+        "SELECT name
+         FROM sqlite_schema
+         WHERE type ='table' 
+         AND name NOT LIKE 'sqlite_%';",
+    )
+    .fetch_all(&db)
+    .await
+    .unwrap();
+
+    for (idx, row) in result.iter().enumerate() {
+        println!("[{}]: {:?}", idx, row.get::<String, &str>("name"));
+    }
+
+    let result = sqlx::query("INSERT INTO insertion_device (poles) VALUES (?)")
+        .bind("80")
+        .execute(&db)
+        .await
+        .unwrap();
+
+    println!("Query result: {:?}", result);
+
+    let ins_results =
+        sqlx::query_as::<_, InsertionDevice>("SELECT uuid, poles from insertion_device")
+            .fetch_all(&db)
+            .await
+            .unwrap();
+
+    for device in ins_results {
+        println!("uuid: {}, poles: {}", device.uuid, device.poles);
+    }
 }
