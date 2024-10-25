99cabc1cf19b8034f80a0eff2c8c3aa3a82f2691
Abigail Emery <abigail.emery@diamond.ac.uk>
2024-10-25 11:23:12 +0100

    Move graphql code into own module

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,6 @@
-use async_graphql::http::GraphiQLSource;
-use async_graphql::{EmptyMutation, EmptySubscription, Object, Schema};
-use async_graphql_axum::{GraphQLRequest, GraphQLResponse};
-use axum::response::Html;
-use axum::routing::get;
-use axum::{extract::Extension, response::IntoResponse, routing::post, Router};
-
-struct Query;
-
-#[Object]
-impl Query {
-    async fn hello(&self) -> &'static str {
-        "hello"
-    }
-    async fn hello_foo(&self, foo: String) -> String {
-        format!("hello {foo}")
-    }
-}
+mod graphql;
 
 #[tokio::main]
 async fn main() {
-    let schema = Schema::build(Query, EmptyMutation, EmptySubscription).finish();
-
-    let app = Router::new()
-        .route("/graphql", post(graphql_handler))
-        .route("/graphiql", get(graphiql_handler))
-        .layer(Extension(schema));
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:8000")
-        .await
-        .unwrap();
-
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn graphql_handler(
-    schema: Extension<Schema<Query, EmptyMutation, EmptySubscription>>,
-    req: GraphQLRequest,
-) -> GraphQLResponse {
-    schema.execute(req.into_inner()).await.into()
-}
-
-async fn graphiql_handler() -> impl IntoResponse {
-    Html(GraphiQLSource::build().endpoint("/graphql").finish())
+    graphql::serve_graphql().await;
 }
